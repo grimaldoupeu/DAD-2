80b4152e503b7434820f312d15a0c053640f4101
grimaldoupeu <grimaldoarredondomartinez@gmail.com>
2024-09-02 12:07:53 -0500

    a

--- ms-catalogo/src/main/java/com/example/mscatalogo/controller/CategoryController.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/controller/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Category")
+@RequestMapping("/category")
 public class CategoryController {
     @Autowired
     private CategoryService CategoryService;
