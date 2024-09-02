79e46051d17b7f641c4ecc7ddf80ffeb059b8574
grimaldoupeu <grimaldoarredondomartinez@gmail.com>
2024-09-02 11:52:23 -0500

    a

--- ms-cliente-server/src/main/java/org/example/mscliente/controller/ClienteController.java
+++ ms-cliente-server/src/main/java/org/example/mscliente/controller/ClienteController.java
@@ -4,10 +4,7 @@ import org.example.mscliente.entity.Cliente;
 import org.example.mscliente.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,23 +15,28 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
+    @PostMapping
     public ResponseEntity<Cliente> guardarClienteResponseEntity(@RequestBody Cliente cliente) {
         return ResponseEntity.ok(clienteService.guardarCliente(cliente));
     }
 
+    @GetMapping
     public ResponseEntity<List<Cliente>> listarClienteResponseEntity(Cliente cliente) {
         return ResponseEntity.ok(clienteService.listarCliente());
     }
 
+    @GetMapping("/{id}")
     public ResponseEntity<Cliente> buscarClientePorIdResponseEntity(@PathVariable(required = true) Integer id) {
         return ResponseEntity.ok(clienteService.buscarClientePorId(id));
     }
 
+    @GetMapping("/{id}")
     public ResponseEntity<Cliente> editarClienteResponseEntity(@PathVariable(required = true) Integer id, @RequestBody Cliente cliente) {
         cliente.setId(id);
         return ResponseEntity.ok(clienteService.editarCliente(cliente));
     }
 
+    @DeleteMapping("/{id}")
     public String eliminarClienteResponseEntity(@PathVariable(required = true) Integer id) {
         clienteService.eliminarCliente(id);
         return "Eliminacion Correcta del Cliente";
